4b3b260161ac3b7df5f2773ccdd2dbbef688a49d
Nandan007S <111338271+Nandan007S@users.noreply.github.com>
2023-06-05 11:01:54 +0530

    conditionals

--- basics.js
+++ basics.js
@@ -13,3 +13,14 @@ console.log(Math.random());
 console.log(1===1); //strict equality
 console.log(1 == "1");
 console.log(1 === '1');
+let rating = 3;
+if(rating === 3){
+    console.log("meets expectations");
+}
+let day = prompt("Enter a day");
+if(day === "Sunday"){
+    console.log("Hurray");
+}
+else{
+    console.log("Meh");
+}
