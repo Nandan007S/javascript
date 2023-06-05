524ab23e0c54b1bb10ca47c5f1b7a73f6a445c59
Nandan007S <111338271+Nandan007S@users.noreply.github.com>
2023-06-05 10:20:15 +0530

    created html file

--- basics.js
+++ basics.js
@@ -4,3 +4,12 @@ console.log(a+b);
 console.log(a/b);
 const num = 10;
 let s1 = "lol";
+let s2 = "Haha that is so funny !!!"; //string immutable
+console.log(s2.slice(-9));
+console.log(`hello ${9+3}`);
+console.log(s2.replace("Haha","Hehe"));
+console.log(Math.round(99.73));
+console.log(Math.random());
+console.log(1===1); //strict equality
+console.log(1 == "1");
+console.log(1 === '1');
